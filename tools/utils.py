1515a87a7e06ade5dab2010cd7f2b836f6df8d61
austin-hoover <ahoover7@tennessee.edu>
2022-05-11 21:57:26 -0400

    Add project and slice functions for ndarrays

--- tools/utils.py
+++ tools/utils.py
@@ -1,9 +1,64 @@
 import numpy as np
 
 
+def avoid_repeats(a, pad=1e-7):
+    """Function written by K. Ruisard to avoid repeating points in array.
+    
+    I'm nots sure why the counter is included.
+    """
+    repeat_idx, = np.where(np.diff(a) == 0)
+    counter = 1
+    for i in reversed(repeat_idx):
+        a[i] -= pad * counter
+        counter += 1
+    return a
+
+
+# Numpy arrays
+# -----------------------------------------------------------------------------
 def max_indices(array):
     """Return indices of maximum element in array."""
     return np.unravel_index(np.argmax(array), array.shape)
+
+
+def slice_array(array, axis=0, ind=0):
+    """Slice array along one or more axes.
+    
+    array : ndarray
+        The array to slice.
+    axis : int or list
+        The axis(axes) along which to slice.
+    ind : int or list
+        Locations on the specified axis(axes).
+    """
+    idx = array.ndim * [slice(None)]
+    if type(axis) is int:
+        axis = [axis]
+    if type(ind) is int:
+        ind = [ind]
+    for i, k in zip(ind, axis):
+        idx[k] = i
+    return array[tuple(idx)]
+
+
+def project(array, axis=0):
+    """Project array onto one or more axes.
+    
+    array : ndarray
+        The distribution.
+    axis : int or list
+        Axis(axes) along which to project the array.
+    """
+    if type(axis) is int:
+        axis = [axis]
+    sum_axis = tuple([k for k in range(array.ndim) if k not in axis])
+    return np.sum(Z, axis=axis)
+
+
+def get_grid_coords(*xi, indexing='ij'):
+    """Return array of shape (N, D), where N is the number of points on 
+    the grid and D is the number of dimensions."""
+    return np.vstack([X.ravel() for X in np.meshgrid(*xi, indexing=indexing)]).T
 
 
 def get_bins(coords, axis=0, n_bins=10, pad=0.1, n_bins_mult=None):
@@ -43,24 +98,3 @@ def get_bins(coords, axis=0, n_bins=10, pad=0.1, n_bins_mult=None):
     # Keep grid values (i.e. bin centers) of bins with nonzero counts.
     gv = bins[np.nonzero(counts)] + 0.5 * (bins[1] - bins[0])
     return gv, idx
-
-
-def project(Z, indices):
-    """Project distribution onto lower-dimensional subspace.
-    
-    Parameters
-    ----------
-    Z : ndarray, n dimensions
-        The distribution.
-    indices : int, list, or tuple
-        The distribution is projected onto these indices.
-        
-    Returns
-    -------
-    ndarray, n - len(indices) dimensions
-        The projection of the distribution.
-    """
-    if type(indices) is int:
-        indices = [indices]
-    axis = tuple([k for k in range(Z.ndim) if k not in indices])
-    return np.sum(Z, axis=axis)
