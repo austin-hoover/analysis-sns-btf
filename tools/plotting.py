ad6ba8bba9e486333e9d6854dcd47ece0bc906e8
austin-hoover <ahoover7@tennessee.edu>
2022-05-15 00:20:20 -0400

    Everything through step 2b is working.

--- tools/plotting.py
+++ tools/plotting.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy import optimize as opt
 from matplotlib import pyplot as plt
 import proplot as pplt
+from . import utils
 
 
 def plot_profiles(ax, image, log=False, scale=0.15, **plot_kws):
@@ -50,3 +51,59 @@ def linear_fit(x, y):
     slope, intercept = popt
     yfit = fit(x, *popt)
     return yfit, slope, intercept
+
+
+def corner(
+    image, 
+    labels=None, 
+    diag_kind='line', 
+    log=False, 
+    fig_kws=None, 
+    diag_kws=None, 
+    **plot_kws
+):
+    n = image.ndim
+    if labels is None:
+        labels = n * ['']
+    if fig_kws is None:
+        fig_kws = dict()
+    fig_kws.setdefault('figwidth', 1.5 * n)
+    fig_kws.setdefault('aligny', True)
+    if diag_kws is None:
+        diag_kws = dict()
+    diag_kws.setdefault('color', 'black')
+    plot_kws.setdefault('ec', 'None')
+    if log:
+        plot_kws['norm'] = 'log'
+        
+    fig, axes = pplt.subplots(
+        nrows=n, ncols=n, sharex=1, sharey=1, 
+        spanx=False, spany=False, **fig_kws
+    )
+    for i in range(n):
+        for j in range(n):
+            ax = axes[i, j]
+            if j > i:
+                ax.axis('off')
+            elif i == j:
+                h = utils.project(image, j)
+                if diag_kind == 'line':
+                    ax.plot(h, **diag_kws)
+                elif diag_kind == 'bar':
+                    ax.bar(h, **diag_kws)
+                elif diag_kind == 'step':
+                    ax.step(h, **diag_kws)
+            else:
+                H = utils.project(image, (j, i))
+                if log:
+                    H = np.ma.masked_less_equal(H, 0)
+                ax.pcolormesh(H.T, **plot_kws)
+    for ax, label in zip(axes[-1, :], labels):
+        ax.format(xlabel=label)
+    for ax, label in zip(axes[1:, 0], labels[1:]):
+        ax.format(ylabel=label)
+    for i in range(n):
+        axes[:-1, i].format(xticklabels=[])
+        if i > 0:
+            axes[i, 1:].format(yticklabels=[])
+    return axes
