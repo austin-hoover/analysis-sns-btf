75e0f87d8327af219cccdcea3335a7f8ab471dea
austin-hoover <ahoover7@tennessee.edu>
2022-05-18 12:22:05 -0400

    Simplify interpolation along one dimension in N-dimensional image

--- tools/image_processing.py
+++ tools/image_processing.py
@@ -2,8 +2,7 @@ import numpy as np
 from scipy import ndimage
 from scipy import interpolate
 
-from .utils import avoid_repeats
-from .utils import get_grid_coords
+from . import utils
 
 
 class CamSettings:
@@ -36,31 +35,29 @@ def thresh(image, thresh=None, val=0, mask=False):
     return im
 
 
-def interpolate_3d(im3d, points, grid, ny, nx, smooth=True):
-    if len(im3d) != len(points):
-        raise ValueError('Must have same number of images and points.')
-    n_frames, ny, nx = im3.shape
-    grid_im_y = np.arange(ny)
-    grid_im_x = np.arange(nx)
-    # Sort by increasing y value (needed for interpolation)
-    idx_sort = np.argsort(points)
-    points = points[idx_sort]
-    im3d = im3d[idx_sort, :, :]
-    # Deal with any repeating points (otherwise interpolate won't work).
-    points = avoid_repeats(points, pad=1e-7)
-    # Apply filter along 1st dimension.
-    if smooth:
-        im3d_smooth = ndimage.median_filter(im3d, size=(3, 1, 1), mode='nearest')
-    else:
-        im3d_smooth = im3d
-    # Interpolate.
-    new_points = get_grid_coords(grid, grid_im_y, grid_im_x)
-    im3d = interpolate.interpn(
-        (points, grid_im_y, grid_im_x), 
-        im3d_smooth, 
-        new_points, 
-        method='linear', 
-        bounds_error=False, 
-        fill_value=0.0,
-    )   
-    return im3d.reshape(len(grid), ny, nx)
+def interp_along_axis(image, x, xnew, axis=0):
+    """Interpolate N-dimensional image along along one axis.
+    
+    It just calls `scipy.interpolate.interp1d`. Before doing so, `x` and
+    `image` are flipped if `x` is decreasing (such as when a slit is moving 
+    backwards). Also, we make sure that `x` has no duplicate elements.
+    
+    Parameters
+    ----------
+    array : ndarray, shape (..., N, ...)
+        An N-D array of real values. The length along the interpolation 
+        axis must be equal to the length of x.
+    x : ndarray, shape (N,)
+        The coordinates of the data points along the interpolation axis.
+    xnew : ndarray, shape (M,)
+        The coordinates at which to evaluate the interpolated image along
+        the interpolation axis.
+    axis : int
+        The axis of `image` along which to interpolate.
+    """
+    idx_sort = np.argsort(x)
+    x = x[idx_sort]
+    image = image[idx_sort, :, :]
+    x = utils.avoid_repeats(x)
+    f = interpolate.interp1d(x, image, axis=0, bounds_error=False, fill_value=0.0)
+    return f(xnew)
